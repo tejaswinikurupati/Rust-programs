235e224a7bbf45295c5e28bb17e27d63e606f85c
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-06-26 14:35:35 +0530

    enumerations

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+enum GenderCategory{
+    Male,Female
+}
+fn main(){
+    let male=GenderCategory::Male;
+    let Female=GenderCategory::Female;
+    println!("{:?}",male);
+    println!("{:?}",female);
+
+}
