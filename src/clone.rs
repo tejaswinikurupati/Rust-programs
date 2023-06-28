1ba5272cf75b6ed8b015b6f3d0fd5312c82f2249
tejaswinikurupati <137766792+tejaswinikurupati@users.noreply.github.com>
2023-06-28 13:09:42 +0530

    Add files via upload

--- src/clone.rs
+++ src/clone.rs
@@ -1,17 +1,5 @@
-fn largest(list:&[i32]) ->&i32 {
-    let mut largest=&list[0];
-    for item in list {
-        if item>largest{
-            largest=item;
-        }
-    }
-    largest
-}
 fn main(){
-    let number_list=vec![34,50,25,65,100];
-    let result=largest(&number_list);
-    println!("the largest number is {}",result);
-    let number_list=vec![102,34,6000,89,54,2,43,8];
-    let result=largest(&number_list);
-    println!("the largest number is {}",result);
-}
+    let s1=String::from("hello");
+    let s2=s1.clone();
+    println!("s1{},s2{}",s1,s2);
+    }
